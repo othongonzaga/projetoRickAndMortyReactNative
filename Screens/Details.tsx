04ef5a698b63871f852b55dc10d278f383883f81
Othon Gonzaga <othoncesar1@hotmail.com>
2024-08-14 11:40:42 -0300

    Fix EsLint issue

--- Screens/Details.tsx
+++ Screens/Details.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Button } from '@react-navigation/elements';
+import {View, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Button} from '@react-navigation/elements';
 
 export default function DetailsScreen() {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
-      <Button onPress={() => navigation.navigate('Home')}>
-        Go to Home
-      </Button>
+      <Button onPress={() => navigation.navigate('Home')}>Go to Home</Button>
     </View>
   );
 }
